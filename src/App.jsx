1d0aee2b58cb92705396aee930747a0a89d208b5
Aashna Sharma <aashnasharma@Aashnas-MacBook-Air.local>
2024-08-14 04:28:49 +0530

    Vercel Issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Navbar from "./components/navbar"
-import Main from "./components/main"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 import AboutMe from "./components/AboutMe"
 import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:bg-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
