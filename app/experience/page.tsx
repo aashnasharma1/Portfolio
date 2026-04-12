1c2788033af8551eeea60ddfe46fb86a2af2aab3
Dishant <dishantkaushal2002@gmail.com>
2026-04-12 22:52:00 +0530

    New Footer and preloader fix

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,19 +1,5 @@
-"use client"
-import Preloader from "@/components/Preloader";
 import Experience from "@/components/sections/Experience";
-import { useState } from "react";
-export default function Page() {
-  const hasVisited = sessionStorage.getItem("hasVisited-Experience");
-  const [loading, setLoading] = useState(!hasVisited);
 
-  const handleVisited = () => {
-    setLoading(false);
-    sessionStorage.setItem("hasVisited-Experience", "true");
-  };
-  return (
-    <>
-      {loading && <Preloader onComplete={handleVisited} />}
-      <Experience />
-    </>
-  );
+export default function Page() {
+  return <Experience />;
 }
